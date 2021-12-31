a9c0eb6085b8d58567db6cdb71dd1878e3d0e025
Deepak Ragu <deepak.ragu@berkeley.edu>
2021-12-30 19:16:41 -0800

    Working on PAragraphNLP.py

--- AutoResearch/src/main/java/jar/ParagraphNLG.py
+++ AutoResearch/src/main/java/jar/ParagraphNLG.py
@@ -16,15 +16,19 @@ def paragraph_nlp(extracted_data_filename : str, url_list_filename : str):
     url_list = read_from_file(path=url_list_filename, root=os.getcwd()).split("\n")
     extracted_data = read_from_file(path=extracted_data_filename, root=os.getcwd()).split("\n")
 
-    word_frequencies, doc_list = clean_up_information()
+    clean_up_information()
+
+    # ToDo: Modify below two lines to get better textual summarization or NLG
+    word_frequencies, doc_list = get_word_freq()
     compiled_info = compile_information(word_frequencies, doc_list)
+
     info_to_write = format_information(compiled_info)
     write_output_to_file(info_to_write=info_to_write, filename="summary.txt")
     print("~Summary~")
     print(info_to_write)
     
 
-def clean_up_information():
+def clean_up_information(): # Include language filters so only english shows up, non-english texts filtered
     global extracted_data
 
     for i in range(len(extracted_data)):
@@ -34,6 +38,8 @@ def clean_up_information():
             sentence = article[j]
             extracted_data[i][j] = sentence.replace("<[^>]*>", "") # remove HTML Tags
 
+def get_word_freq():
+    global extracted_data
 
     nlp = spacy.load('en_core_web_sm')
     doc_list = [[nlp(word) for word in article] for article in extracted_data]
@@ -75,12 +81,39 @@ def compile_information(word_freq : dict, cleaned_info : list) -> list:
     nlp = spacy.load('en_core_web_sm')
     doc_list = [[nlp(word) for word in article] for article in extracted_data]
 
-    weighted_doc_list = []        
+    # weighted_doc_list = []        
+    # for i in range(len(doc_list)):
+    #     article = doc_list[i]
+    #     sentence_tokens = [sent for sent in article]
+    #     sentence_scores = {}
+    #     for sent in sentence_tokens:
+    #         for word in sent:
+    #             if word.text.lower() in word_freq.keys():
+    #                 if sent not in sentence_scores.keys():                            
+    #                     sentence_scores[sent]=word_freq[word.text.lower()]
+    #                 else:
+    #                     sentence_scores[sent]+=word_freq[word.text.lower()]
+    #     sentence_tokens.sort(key=lambda sent: sentence_scores[sent] if sent in sentence_scores else -1, reverse=True)
+    #     weighted_doc_list.append(sentence_tokens)
+    
+    # compiled_info = []
+    # for i in range(len(extracted_data)): 
+    #     compiled_info.append(weighted_doc_list[i][min(i, len(weighted_doc_list[i])-1)])
+
+    # return compiled_info
+
+
+
+
+    weighted_doc_list = []       
+    sentence_scores = {}
+    all_sentences = []
+
     for i in range(len(doc_list)):
         article = doc_list[i]
-        # print("article: ", article)
         sentence_tokens = [sent for sent in article]
-        sentence_scores = {}
+        all_sentences += sentence_tokens
+        
         for sent in sentence_tokens:
             for word in sent:
                 if word.text.lower() in word_freq.keys():
@@ -88,17 +121,34 @@ def compile_information(word_freq : dict, cleaned_info : list) -> list:
                         sentence_scores[sent]=word_freq[word.text.lower()]
                     else:
                         sentence_scores[sent]+=word_freq[word.text.lower()]
-        # print("sentence_tokens: ", sentence_tokens)
-        # print("sentence_scores: ", sentence_scores)
+
         sentence_tokens.sort(key=lambda sent: sentence_scores[sent] if sent in sentence_scores else -1, reverse=True)
         weighted_doc_list.append(sentence_tokens)
     
-    # print("weighted_doc_list: ", weighted_doc_list)
-    compiled_info = []
-    for i in range(len(extracted_data)): 
-        compiled_info.append(weighted_doc_list[i][min(i, len(weighted_doc_list[i])-1)])
+    # compiled_info = []
+    # for i in range(0.3*len(extracted_data)): 
+    #     compiled_info.append(weighted_doc_list[i][min(i, len(weighted_doc_list[i])-1)])
 
-    return compiled_info
+    # return compiled_info
+
+    # print("word_freq: ", word_freq)
+    # print("sentence_scores: ", sentence_scores)
+
+
+
+    all_sentences.sort(key=lambda sent: sentence_scores[sent] if sent in sentence_scores else -1, reverse=True)
+    return all_sentences[0:min(8, len(all_sentences))]
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -109,7 +159,7 @@ def create_citations() -> list:
     return [str(i+1) for i in range(len(url_list))]
 
 def format_information(compiled_info : list) -> str:
-    print("compiled_info: ", compiled_info)
+    # print("compiled_info: ", compiled_info)
     formatted_info = []
     for i in range(len(compiled_info)):
         formatted_info.append(compiled_info[i].text)
@@ -138,6 +188,80 @@ def write_to_file(contents, path, root=""):
         write_file.write(contents + '\n')
         write_file.close()
     return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
